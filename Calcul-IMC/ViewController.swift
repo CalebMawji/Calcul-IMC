e8e81519ffdf1081af30ba80647f1f9620758e88
CalebMawji <cmawji@tfs.ca>
2023-04-17 09:01:25 -0400

    Initial Commit

---
+++ Calcul-IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calcul-IMC
+//
+//  Created by Caleb Mawji on 2023-04-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
